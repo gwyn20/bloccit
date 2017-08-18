6affa77445d5b1ea7d49915559870d1a550f9bd7
Chris Jones <gwyn20@gmail.com>
2017-08-17 21:07:23 -0400

    Checkpoint 17 Introduction to CRUD: Completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
 
-  get 'welcome/about'
+    resources :posts
 
-  root 'welcome#index'
+    get 'about' => 'welcome#about'
+
+    root 'welcome#index'
 
 end
