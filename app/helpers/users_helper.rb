c8b19da7c23dce7f7b46867b64d8b6cd40005633
Chris Jones <gwyn20@gmail.com>
2017-09-14 03:18:21 -0400

    Assignment 31 Public Profiles: Completed

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,13 @@
 module UsersHelper
+    def user_has_made_posts?
+       @user.posts.exists?
+    end
+
+    def user_has_made_comments?
+        @user.comments.exists?
+    end
+
+    def user_has_favorited_posts?
+        @user.favorites.exists?
+    end
 end
